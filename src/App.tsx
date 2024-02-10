65b35d5e5bc619d23e4f6bd47423730eaec7a20d
ngustavo80 <ngustavo80@hotmail.com>
2024-02-10 09:53:23 -0300

    App style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import logo from './assets/logoNLWExpert.svg'
 import { NewNoteCard } from './components/NewNoteCard'
 import { NoteCard } from './components/NoteCard'
 
+const content = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non a aut, odio, soluta ea culpa delectus amet dolores impedit accusamus beatae at, reprehenderit odit vel explicabo. Sit vel qui rem?'
+
 export function App() {
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -20,11 +22,9 @@ export function App() {
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard 
+          note={{date: new Date(), content: content}}
+        />
       </div>
     </div>
   )
--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,19 +1,53 @@
-export function NoteCard() {
-  return (
-    <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
-      <span className='text-sm font-medium text-slate-300'>
-        Adicionar nota
-      </span>
-      <p className='text-sm leading-6 text-slate-400'>
-        Grave uma nota em áudio que será convertida para 
-        texto automaticamente.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit eum \
-        quasi ducimus fugit qui. Sed aspernatur, repellat amet minus ratione 
-        cum est, quibusdam earum fugiat, ducimus atque culpa assumenda 
-        laboriosam!
-      </p>
+import * as Dialog from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
 
-      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/75 to-black/0 pointer-events-none' />
-    </div>
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note } : NoteCardProps) {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className='rounded-md bg-slate-800 text-left p-5 gap-3 flex flex-col outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+        <span className='text-sm font-medium text-slate-300'>
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true
+          })}
+        </span>
+        <p className='text-sm leading-6 text-slate-400'>
+          {note.content}
+        </p>
+
+        <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/75 to-black/0 pointer-events-none' />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/50' />
+        <Dialog.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] overflow-hidden bg-slate-700 rounded-md flex flex-col outline-none'>
+          <Dialog.Close className='absolute right-0 p-1.5 text-slate-500 bg-slate-800 hover:text-slate-100'>
+            <X className='size-5' />
+          </Dialog.Close>
+          
+          <div className='flex flex-1 flex-col gap-3 p-5'>
+            <span className='text-sm font-medium text-slate-300'>
+              {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+            </span>
+            <p className='text-sm leading-6 text-slate-400'>
+              {note.content}
+            </p>
+          </div>
+
+          <button type='button' className='w-full bg-slate-800 py-4 font-medium text-center text-sm text-slate-300 outline-none group'>
+            Deseja <span className='text-red-400 group-hover:underline'>apagar essa nota?</span>
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
